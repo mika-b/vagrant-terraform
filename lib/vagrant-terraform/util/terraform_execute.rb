4113fdd66c0c53f5c0931b24562339ae085bd67d
Mika Båtsman <mika.batsman@gmail.com>
2025-03-12 11:33:45 +0200

    Minor cleanup

--- lib/vagrant-terraform/util/terraform_execute.rb
+++ lib/vagrant-terraform/util/terraform_execute.rb
@@ -10,14 +10,14 @@ module VagrantPlugins
         def terraform_execute(env, command)
           env[:machine_tf_dir] = ".vagrant/terraform/#{env[:machine].id}" if env[:machine_tf_dir].nil?
           Dir.mkdir(env[:machine_tf_dir]) unless File.exist?(env[:machine_tf_dir])
+
           stdout, stderr, status = Open3.capture3(command, :chdir=>env[:machine_tf_dir])
-          if stderr != ""
-            if env[:machine].provider_config.debug
+
+          if !stderr.empty? && env[:machine].provider_config.debug
               env[:ui].info("terraform command: #{command}")
               env[:ui].info("terraform stdout: #{stdout}")
               env[:ui].info("terraform stderr: #{stderr}")
               env[:ui].info("terraform status: #{status}")
-            end
           end
 
           if status != 0
